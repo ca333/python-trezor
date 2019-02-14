42b6691ba5ab2ef941345146c50c56abeca7922c
ca333 <ca333@keemail.me>
2019-02-14 21:08:03 +0100

    add Komodo test [fix]

--- trezorlib/tests/device_tests/test_msg_signtx_komodo.py
+++ trezorlib/tests/device_tests/test_msg_signtx_komodo.py
@@ -22,11 +22,8 @@ from trezorlib.tools import parse_path
 from ..support.tx_cache import tx_cache
 from .common import TrezorTest
 
-TX_API = tx_cache("Komodo Mainnet")
+TX_API = tx_cache("Komodo")
 
-# TXHASH_fb2847 = bytes.fromhex(
-#     "fb2847e4f3de789d4cfa0e0dcc96ad47f68c2ea2897013adbe40974c1e6dbe39"
-# )
 TXHASH_339c3e = bytes.fromhex(
     "339c3e78610e229f65ebc3fa722016fcb9fbde7bc196d2d876604f5257ada19c"
 )
@@ -34,74 +31,6 @@ TXHASH_339c3e = bytes.fromhex(
 
 @pytest.mark.komodo
 class TestMsgSigntxKomodo(TrezorTest):
-    # ca333 - v1 Tests deactivated since v4 is mandatory
-    # def test_one_one_fee_overwinter(self):
-    #     self.setup_mnemonic_allallall()
-    #
-    #     # prevout: fb2847e4f3de789d4cfa0e0dcc96ad47f68c2ea2897013adbe40974c1e6dbe39:1
-    #     # input 1: 16.82116 KMD
-    #
-    #     inp1 = proto.TxInputType(
-    #         address_n=parse_path(
-    #             "m/Komodo Mainnet/0h/0/0"
-    #         ),  # RLCR1rMjGUV33ReXi4ptQq7JMhUpUai5tg
-    #         amount=1682116000,
-    #         prev_hash=TXHASH_fb2847,
-    #         prev_index=1,
-    #     )
-    #
-    #     out1 = proto.TxOutputType(
-    #         address="RQC4SesZiZ7WiBnvpCTSLkikhfqLbpNT7Q",
-    #         amount=1682116000 - 10000,
-    #         script_type=proto.OutputScriptType.PAYTOADDRESS,
-    #     )
-    #
-    #     with self.client:
-    #         self.client.set_expected_responses(
-    #             [
-    #                 proto.TxRequest(
-    #                     request_type=proto.RequestType.TXINPUT,
-    #                     details=proto.TxRequestDetailsType(request_index=0),
-    #                 ),
-    #                 proto.TxRequest(
-    #                     request_type=proto.RequestType.TXOUTPUT,
-    #                     details=proto.TxRequestDetailsType(request_index=0),
-    #                 ),
-    #                 proto.ButtonRequest(code=proto.ButtonRequestType.ConfirmOutput),
-    #                 proto.ButtonRequest(code=proto.ButtonRequestType.SignTx),
-    #                 proto.TxRequest(
-    #                     request_type=proto.RequestType.TXINPUT,
-    #                     details=proto.TxRequestDetailsType(request_index=0),
-    #                 ),
-    #                 proto.TxRequest(
-    #                     request_type=proto.RequestType.TXOUTPUT,
-    #                     details=proto.TxRequestDetailsType(request_index=0),
-    #                 ),
-    #                 proto.TxRequest(request_type=proto.RequestType.TXFINISHED),
-    #             ]
-    #         )
-    #
-    #         details = proto.SignTx(
-    #             version=1,
-    #             overwintered=False,
-    #             #version_group_id=0x3C48270,
-    #             branch_id=0x5BA81B19,
-    #         )
-    #         _, serialized_tx = btc.sign_tx(
-    #             self.client,
-    #             "Komodo Mainnet",
-    #             [inp1],
-    #             [out1],
-    #             details=details,
-    #             prev_txes=TX_API,
-    #         )
-    #
-    #     # Accepted by network: tx 66160449b3bdc499d2a7e711139febbb4f8a25f195c846f5d42db86db77e73ff
-    #     assert (
-    #         serialized_tx.hex()
-    #         == "010000000139be6d1e4c9740bead137089a22e8cf647ad96cc0d0efa4c9d78def3e44728fb010000006b483045022100de573ae38d31c67560410774f9ec065ef2cf4222d4ce7667745e895d2f4f9cfe0220333bd1d480770e06cc75bca990d4e811f5ffa5b0e69063f7ebc62bbaa618a5dd012102f1bca007fabf60ff62145fa63835dac81dfaf6c5d390d9a7a2d1bf4360fc410fffffffff0190e64264000000001976a914a3916ee0099881a0e7da8ab5eb9395a9039bf4c388ac00000000"
-    #     )
-
     def test_one_one_fee_sapling(self):
         self.setup_mnemonic_allallall()
 
@@ -110,7 +39,7 @@ class TestMsgSigntxKomodo(TrezorTest):
 
         inp1 = proto.TxInputType(
             address_n=parse_path(
-                "m/Komodo Mainnet/0h/0/0"
+                "m/Komodo/0h/0/0"
             ),  # RDvyC66RQf7HKkUB5zyLKJhitV4ibzkKF5
             amount=299990000,
             prev_hash=TXHASH_339c3e,
@@ -156,7 +85,7 @@ class TestMsgSigntxKomodo(TrezorTest):
             )
             _, serialized_tx = btc.sign_tx(
                 self.client,
-                "Komodo Mainnet",
+                "Komodo",
                 [inp1],
                 [out1],
                 details=details,
